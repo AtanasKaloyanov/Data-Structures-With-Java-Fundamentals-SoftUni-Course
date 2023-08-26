1d151f1b74a9d4b82fbfdd5d3a578a7c47db9967
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2023-08-26 21:35:20 +0300

    This is an example for ArrayList class with Node class inside it.

---
+++ T01DataStructuresAndComplexity/ArrayList.java
@@ -0,0 +1,11 @@
+package T01DataStructuresAndComplexity;
+
+public class ArrayList {
+    private static class Node {
+        private int element;
+        private Node next;
+    }
+
+    private Node head;
+
+}
