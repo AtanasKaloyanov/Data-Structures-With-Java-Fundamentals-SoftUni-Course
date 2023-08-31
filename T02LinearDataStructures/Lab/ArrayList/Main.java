af6c3bff948b91fbefdf7e0dc7753bbfd0196441
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2023-08-31 23:15:15 +0300

    ArrayList implementation

--- T02LinearDataStructures/Lab/ArrayList/Main.java
+++ T02LinearDataStructures/Lab/ArrayList/Main.java
@@ -1,28 +1,31 @@
 package T02LinearDataStructures.Lab.ArrayList;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) {
-        List<Integer> numbers = new ArrayList<>();
-        numbers.add(1);
-        numbers.add(2);
-        numbers.add(3);
+        int[] array = new int[8];
+        array[0] = 3;
+        array[1] = 5;
+        array[2] = 7;
+        array[3] = 9;
 
-        // size
-        System.out.println(numbers.size());
+        int index = 0;
+        int element = 1;
 
-        // isEmpty
-        System.out.println(numbers.isEmpty());
+        int lastElement = array[3];
 
-        // get
-        System.out.println(numbers.get(0));
+        for (int i = 3; i > index; i--) {
+             array[i] = array[i - 1];
+        }
 
-        // set
-        System.out.println(numbers.set(0, 10));
-        numbers.forEach( (element) -> System.out.print(element + " "));
+        array[index] = element;
 
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
 
+        System.out.println();
+        System.out.println(lastElement);
     }
 }
---
+++ T02LinearDataStructures/Lab/ArrayList/Test.java
@@ -0,0 +1,44 @@
+package T02LinearDataStructures.Lab.ArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+
+        // int size()
+        System.out.println(numbers.size());
+
+        // boolean isEmpty()
+        System.out.println(numbers.isEmpty());
+
+        // E get(int index)
+        System.out.println(numbers.get(0));
+
+        // E set(int index, E element)
+        System.out.println(numbers.set(0, 10));
+        numbers.forEach( (element) -> System.out.print(element + " "));
+
+        // E remove(int index)
+        System.out.println();
+        System.out.println(numbers.remove(0));
+
+        // void add(int index, E element)
+
+        numbers.add(2, 4);
+        numbers.forEach( (element) -> System.out.print(element + " "));
+
+        // boolean contains(Object o)
+        System.out.println();
+        System.out.println(numbers.contains(20));
+
+        // int indexOf(Object o)
+        System.out.println(numbers.indexOf(4));
+
+
+    }
+}
