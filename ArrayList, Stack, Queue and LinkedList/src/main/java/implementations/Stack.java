f8227f50faa8d57ad7461a2a7ad6dd158c4bc460
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2023-09-04 17:46:15 +0300

    This is a Stack implementation

--- ArrayList, Stack, Queue and LinkedList/src/main/java/implementations/Stack.java
+++ ArrayList, Stack, Queue and LinkedList/src/main/java/implementations/Stack.java
@@ -5,34 +5,85 @@ import interfaces.AbstractStack;
 import java.util.Iterator;
 
 public class Stack<E> implements AbstractStack<E> {
+    private Node<E> top;
+    private int size;
+
+    private static class Node<E> {
+        private Node<E> previous;
+        private E element;
+
+        public Node(E element) {
+            this.element = element;
+        }
+    }
+
+    public Stack() {
+
+    }
 
     @Override
     public void push(E element) {
+        Node<E> newTop = new Node<>(element);
+        Node<E> oldTop = this.top;
+        this.top = newTop;
+        newTop.previous = oldTop;
 
+        this.size++;
     }
 
     @Override
     public E pop() {
-        return null;
+        ensureNotEmpty();
+
+        Node<E> oldHead = this.top;
+        Node<E> newHead = oldHead.previous;
+
+        oldHead.previous = null;
+        this.top = newHead;
+
+        this.size--;
+
+        return oldHead.element;
     }
 
     @Override
     public E peek() {
-        return null;
+        ensureNotEmpty();
+        return this.top.element;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.size == 0;
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        Node<E> current = this.top;
+        return new Iterator<E>() {
+
+            @Override
+            public boolean hasNext() {
+                return current == null;
+            }
+
+            @Override
+            public E next() {
+                Node<E> node = current;
+                node = node.previous;
+                return node.element;
+            }
+        };
+    }
+
+    private void ensureNotEmpty() {
+        if (isEmpty()) {
+            throw new IllegalStateException(String.format("The stack is empty."));
+        }
     }
 }
