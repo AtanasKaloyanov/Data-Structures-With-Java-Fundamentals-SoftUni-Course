5dcadefce1c35f1ae7b76f1182da401c78ba398a
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2023-09-05 16:15:24 +0300

    2 projects

---
+++ T02/ArrayList, Stack, Queue and LinkedList/src/main/java/Main.java
@@ -0,0 +1,14 @@
+import implementations.ArrayList;
+import implementations.Stack;
+import interfaces.AbstractStack;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        AbstractStack<Integer> stack = new Stack<>();
+        stack.size();
+
+
+    }
+}
