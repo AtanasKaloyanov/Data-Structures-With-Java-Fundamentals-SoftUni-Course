88649188118aa893fd52efdc0125bdae910af732
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2023-09-08 18:38:19 +0300

    This is the solution for the problem "Balanced parentheses"

--- T02 LDS/Exercise/5 Linear Data Structures/src/main/java/implementations/BalancedParentheses.java
+++ T02 LDS/Exercise/5 Linear Data Structures/src/main/java/implementations/BalancedParentheses.java
@@ -2,6 +2,8 @@ package implementations;
 
 import interfaces.Solvable;
 
+import java.util.ArrayDeque;
+
 public class BalancedParentheses implements Solvable {
     private String parentheses;
 
@@ -11,6 +13,36 @@ public class BalancedParentheses implements Solvable {
 
     @Override
     public Boolean solve() {
-        return null;
+        String input = this.parentheses;
+        java.util.ArrayDeque<Character> stack = new ArrayDeque<>();
+        boolean isBalanced = false;
+
+        for (int i = 0; i < input.length(); i++) {
+            char currentChar = input.charAt(i);
+            if (currentChar == '(' || currentChar == '[' || currentChar == '{') {
+                stack.push(currentChar);
+            } else if (currentChar == ')' || currentChar == ']' || currentChar == '}') {
+
+                if (stack.isEmpty()) {
+                    isBalanced = false;
+                    break;
+                }
+
+                char currentOpenBracket = stack.pop();
+
+                if (currentOpenBracket == '(' && currentChar == ')') {
+                    isBalanced = true;
+                } else if (currentOpenBracket == '[' && currentChar == ']') {
+                    isBalanced = true;
+                } else if (currentOpenBracket == '{' && currentChar == '}') {
+                    isBalanced = true;
+                } else {
+                    isBalanced = false;
+                    break;
+                }
+            }
+        }
+
+        return isBalanced;
     }
 }
