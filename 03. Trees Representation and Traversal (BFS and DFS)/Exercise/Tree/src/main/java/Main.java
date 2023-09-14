881618f36241c8d7b026457f2e9d5182f0f52048
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2023-09-14 22:41:50 +0300

    This is the second solution for the problem "The Matrix"

--- 03. Trees Representation and Traversal (BFS and DFS)/Exercise/Tree/src/main/java/Main.java
+++ 03. Trees Representation and Traversal (BFS and DFS)/Exercise/Tree/src/main/java/Main.java
@@ -1,20 +1,7 @@
-import implementations.TheMatrix;
-
 public class Main {
     public static void main(String[] args) {
-        char[][] matrix = {
-                {'o', 'o', 'o', 'o', 'o', 'o'},
-                {'o', 'o', 'o', '1', 'o', 'o'},
-                {'o', 'o', '1', 'o', '1', '1'},
-                {'o', '1', '1', 'w', '1', 'o'},
-                {'1', 'o', 'o', 'o', 'o', 'o'}
-        };
 
-        TheMatrix theMatrix = new TheMatrix(matrix, 'x', 0, 0);
 
-        theMatrix.solve();
-
-        System.out.println(theMatrix.toOutputString());
     }
 }
 
--- 03. Trees Representation and Traversal (BFS and DFS)/Exercise/Tree/src/main/java/implementations/TheMatrix.java
+++ 03. Trees Representation and Traversal (BFS and DFS)/Exercise/Tree/src/main/java/implementations/TheMatrix.java
@@ -1,5 +1,8 @@
 package implementations;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class TheMatrix {
     private char[][] matrix;
     private char fillChar;
@@ -15,8 +18,41 @@ public class TheMatrix {
         this.toBeReplaced = this.matrix[this.startRow][this.startCol];
     }
 
+    // bfs 
+//    public void solve() {
+//        fillMatrix(this.startRow, this.startCol);
+//    }
+
+    // dfs
     public void solve() {
-        fillMatrix(this.startRow, this.startCol);
+        Deque<int[]> coordinates = new ArrayDeque<>();
+        coordinates.add(new int[]{this.startRow, this.startCol});
+
+        while (!coordinates.isEmpty()) {
+            int[] position = coordinates.poll();
+            int row = position[0];
+            int col = position[1];
+
+            this.matrix[row][col] = this.fillChar;
+
+            if (isInBounds(row + 1, col) && this.matrix[row + 1][col] == this.toBeReplaced) {
+                coordinates.add(new int[]{row + 1, col});
+            }
+            if (isInBounds(row - 1, col) && this.matrix[row - 1][col] == this.toBeReplaced) {
+                coordinates.add(new int[]{row - 1, col});
+            }
+            if (isInBounds(row, col + 1) && this.matrix[row][col + 1] == this.toBeReplaced) {
+                coordinates.add(new int[]{row, col + 1});
+            }
+            if (isInBounds(row, col - 1) && this.matrix[row][col - 1] == this.toBeReplaced) {
+                coordinates.add(new int[]{row, col - 1});
+            }
+
+        }
+    }
+
+    private boolean isInBounds(int row, int col) {
+        return row >= 0 && row < this.matrix.length && col >= 0 && col < this.matrix[row].length;
     }
 
     private void fillMatrix(int startRow, int startCol) {
@@ -42,7 +78,6 @@ public class TheMatrix {
             fillMatrix(startRow, startCol + 1);
         }
     }
-
 
     public String toOutputString() {
         StringBuilder sb = new StringBuilder();
