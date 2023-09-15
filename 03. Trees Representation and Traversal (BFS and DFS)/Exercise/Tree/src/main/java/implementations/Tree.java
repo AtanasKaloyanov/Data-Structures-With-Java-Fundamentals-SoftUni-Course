12dad2bfef7c9f4bb57fada7c4339c0467e2ab6e
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2023-09-15 23:49:00 +0300

    Solutions for the problem "Test Tree as String" and "Test Tree Creation"

--- 03. Trees Representation and Traversal (BFS and DFS)/Exercise/Tree/src/main/java/implementations/Tree.java
+++ 03. Trees Representation and Traversal (BFS and DFS)/Exercise/Tree/src/main/java/implementations/Tree.java
@@ -2,33 +2,72 @@ package implementations;
 
 import interfaces.AbstractTree;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
 
 public class Tree<E> implements AbstractTree<E> {
+    private E key;
+    private Tree<E> parent;
+    private List<Tree<E>> children;
+
+    public Tree(E key /*,Tree<E>... children*/) {
+        this.key = key;
+        this.children = new ArrayList<>();
+
+//        for (Tree<E> currentChild : children) {
+//            this.children.add(currentChild);
+//            currentChild.setParent(this);
+//        }
+    }
 
     @Override
     public void setParent(Tree<E> parent) {
-
+        this.parent = parent;
     }
 
     @Override
     public void addChild(Tree<E> child) {
-
+        this.children.add(child);
     }
 
     @Override
     public Tree<E> getParent() {
-        return null;
+        return this.parent;
     }
 
     @Override
     public E getKey() {
-        return null;
+        return this.key;
     }
 
     @Override
     public String getAsString() {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.key).append(System.lineSeparator());
+
+        int counter = 0;
+
+        dfsRecursion(this, sb, counter);
+
+        return sb.toString().trim();
+    }
+
+    private void dfsRecursion(Tree<E> tree, StringBuilder sb, int counter) {
+        if (tree == null) {
+            return;
+        }
+
+        counter++;
+
+        for (Tree<E> current : tree.children) {
+            sb.append("  ".repeat(Math.max(0, counter)));
+            sb.append(current.key);
+            sb.append(System.lineSeparator());
+            dfsRecursion(current, sb, counter);
+        }
+
     }
 
     @Override
