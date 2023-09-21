cf6909b3f51bcfdfad737095d42312815dd0bebc
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2023-09-21 23:10:52 +0300

    This is the Binary Tree implementation

--- 04. Heaps, BST/Lab/Binary Trees, Heaps, BST/src/main/java/implementations/BinarySearchTree.java
+++ 04. Heaps, BST/Lab/Binary Trees, Heaps, BST/src/main/java/implementations/BinarySearchTree.java
@@ -3,39 +3,94 @@ package implementations;
 import interfaces.AbstractBinarySearchTree;
 
 public class BinarySearchTree<E extends Comparable<E>> implements AbstractBinarySearchTree<E> {
+    private Node<E> root;
+
+    public BinarySearchTree() {
+
+    }
+
+    public BinarySearchTree(Node<E> root) {
+        this.root = root;
+    }
 
     @Override
     public void insert(E element) {
+        Node<E> newNode = new Node<>(element);
+        if (this.root == null) {
+            this.root = newNode;
+        } else {
+            Node<E> parent = null;
+            Node<E> current = this.root;
 
+            while (current != null) {
+                int compare = current.value.compareTo(element);
+                if (compare > 0) {
+                    parent = current;
+                    current = current.leftChild;
+                } else if (compare < 0){
+                    parent = current;
+                    current = current.rightChild;
+                } else {
+                    return;
+                }
+            }
+
+            if (parent.value.compareTo(element) > 0){
+                parent.leftChild = newNode;
+            } else {
+                parent.rightChild = newNode;
+            }
+        }
     }
 
     @Override
     public boolean contains(E element) {
-        return false;
+        Node<E> current = this.root;
+        while (current != null){
+            if (element.compareTo(current.value) < 0){
+                current = current.leftChild;
+            } else if (element.compareTo(current.value) > 0){
+                current = current.rightChild;
+            } else {
+                break;
+            }
+        }
+        return current != null;
     }
 
     @Override
     public AbstractBinarySearchTree<E> search(E element) {
-        return null;
+        Node<E> current = this.root;
+
+        while (current != null){
+            if (element.compareTo(current.value) < 0) {
+                current = current.leftChild;
+            } else if (element.compareTo(current.value) > 0) {
+                current = current.rightChild;
+            } else {
+                break;
+            }
+        }
+        return new BinarySearchTree<>(current);
     }
 
     @Override
     public Node<E> getRoot() {
-        return null;
+        return this.root;
     }
 
     @Override
     public Node<E> getLeft() {
-        return null;
+        return this.root.leftChild;
     }
 
     @Override
     public Node<E> getRight() {
-        return null;
+        return this.root.rightChild;
     }
 
     @Override
     public E getValue() {
-        return null;
+        return this.root.value;
     }
 }
