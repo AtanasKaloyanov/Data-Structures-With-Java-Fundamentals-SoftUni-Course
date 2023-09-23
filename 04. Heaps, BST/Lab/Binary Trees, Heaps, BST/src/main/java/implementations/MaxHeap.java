06baae88ae1e5784e51779b17b0d9dddc3b058a0
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2023-09-23 19:11:23 +0300

    This is the MaxHeap implementation

--- 04. Heaps, BST/Lab/Binary Trees, Heaps, BST/src/main/java/implementations/MaxHeap.java
+++ 04. Heaps, BST/Lab/Binary Trees, Heaps, BST/src/main/java/implementations/MaxHeap.java
@@ -7,19 +7,54 @@ import java.util.Collections;
 import java.util.List;
 
 public class MaxHeap<E extends Comparable<E>> implements Heap<E> {
+     private List<E> elements;
+
+     public MaxHeap() {
+         this.elements = new ArrayList<>();
+     }
 
     @Override
     public int size() {
-        return 0;
+        return this.elements.size();
     }
 
     @Override
     public void add(E element) {
-
+      this.elements.add(element);
+      this.heapify(this.size() - 1);
     }
+
+    private void heapify(int index) {
+         while (hasParent(index) && less(parent(getMiddleIndex(index)), this.elements.get(index))) {
+             int middleIndex = getMiddleIndex(index);
+             Collections.swap(this.elements, middleIndex, index);
+             index = middleIndex;
+         }
+    }
+
+    private int getMiddleIndex(int index) {
+         return (index - 1) / 2;
+    }
+
+    private E parent(int index) {
+         return this.elements.get(index);
+    }
+
+    private boolean less(E first, E second) {
+         return first.compareTo(second) < 0;
+    }
+
+    private boolean hasParent(int index) {
+         return index > 0;
+    }
+
 
     @Override
     public E peek() {
-        return null;
+        if (this.size() == 0) {
+            throw new IllegalStateException("The size of the elements is 0.");
+        }
+
+        return this.elements.get(0);
     }
 }
