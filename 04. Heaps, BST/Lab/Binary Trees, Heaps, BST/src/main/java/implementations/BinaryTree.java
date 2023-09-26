69c82a999299e5e0c76c5d61e393c672d9205076
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2023-09-26 23:47:00 +0300

    Binary Tree modification

--- 04. Heaps, BST/Lab/Binary Trees, Heaps, BST/src/main/java/implementations/BinaryTree.java
+++ 04. Heaps, BST/Lab/Binary Trees, Heaps, BST/src/main/java/implementations/BinaryTree.java
@@ -39,48 +39,16 @@ public class BinaryTree<E> implements AbstractBinaryTree<E> {
         this.key = key;
     }
 
-//    @Override
-//    public String asIndentedPreOrder(int indent) {
-//        StringBuilder result = new StringBuilder();
-//        result.append(this.key).append(System.lineSeparator());
-//
-//        appending(this.left, result, indent);
-//        appending(this.right, result, indent);
-//
-//        return result.toString().trim();
-//    }
-//
-//    private void appending(BinaryTree<E> tree, StringBuilder result, int indent) {
-//        Queue<BinaryTree<E>> queue = new ArrayDeque<>();
-//        queue.offer(tree);
-//
-//        int counter = 1;
-//
-//        while (!queue.isEmpty()) {
-//            BinaryTree<E> current = queue.poll();
-//
-//            if (counter % 2 == 0) {
-//                indent++;
-//            }
-//
-//            for (int i = 0; i <= indent; i++) {
-//                result.append("  ");
-//            }
-//
-//            counter++;
-//
-//            result.append(current.key);
-//            result.append(System.lineSeparator());
-//
-//            if (current.left != null) {
-//                queue.offer(current.left);
-//            }
-//
-//            if (current.right != null) {
-//                queue.offer(current.right);
-//            }
-//        }
-//    }
+
+     /*
+          17
+            9
+              3
+              11
+            25
+              20
+              21
+     */
 
     //                     17
 
@@ -92,31 +60,34 @@ public class BinaryTree<E> implements AbstractBinaryTree<E> {
     public String asIndentedPreOrder(int indent) {
         StringBuilder result = new StringBuilder();
 
-        String padding = createPadding(indent) + this.getKey();
-        result.append(padding);
+        result.append(createPadding(indent)).append(this.key);
 
-        if (this.getLeft() != null) {
-            String preOrder = this.getLeft().asIndentedPreOrder(indent + 2);
-            result.append(System.lineSeparator()).append(preOrder);
+        if (this.left != null) {
+            String preOrder = getLeft().asIndentedPreOrder(indent + 2);
+            result.append(System.lineSeparator());
+            result.append(preOrder);
         }
 
-        if (this.getRight() != null) {
-            String preOrder = this.getRight().asIndentedPreOrder(indent + 2);
-            result.append(System.lineSeparator()).append(preOrder);
+        if (this.right != null) {
+            String preOrder = getRight().asIndentedPreOrder(indent + 2);
+            result.append(System.lineSeparator());
+            result.append(preOrder);
+
         }
 
         return result.toString();
     }
 
     private String createPadding(int indent) {
-        StringBuilder builder = new StringBuilder();
+        StringBuilder indentResult = new StringBuilder();
 
         for (int i = 0; i < indent; i++) {
-            builder.append(" ");
+            indentResult.append(" ");
         }
 
-        return builder.toString();
+        return indentResult.toString();
     }
+
 
     @Override
     public List<AbstractBinaryTree<E>> preOrder() {
@@ -127,17 +98,17 @@ public class BinaryTree<E> implements AbstractBinaryTree<E> {
         return result;
     }
 
-    private void preOrderRecc(BinaryTree<E> root, List<AbstractBinaryTree<E>> result) {
-        if (root == null) {
-            return;
+    private void preOrderRecc(BinaryTree<E> current, List<AbstractBinaryTree<E>> result) {
+
+        result.add(current);
+
+        if (current.left != null) {
+            preOrderRecc(current.left, result);
         }
 
-        result.add(root);
-
-        BinaryTree<E> left = root.left;
-        preOrderRecc(left, result);
-        BinaryTree<E> right = root.right;
-        preOrderRecc(right, result);
+        if (current.right != null) {
+            preOrderRecc(current.right, result);
+        }
     }
 
     //                     17
@@ -157,16 +128,15 @@ public class BinaryTree<E> implements AbstractBinaryTree<E> {
     }
 
     private void inOrderRecc(BinaryTree<E> current, List<AbstractBinaryTree<E>> result) {
-        if (current == null) {
-            return;
+
+        if (current.left != null) {
+            inOrderRecc(current.left, result);
         }
 
-        BinaryTree<E> left = current.left;
-        inOrderRecc(left, result);
         result.add(current);
-
-        BinaryTree<E> right = current.right;
-        inOrderRecc(right, result);
+        if (current.right != null) {
+            inOrderRecc(current.right, result);
+        }
     }
 
     //                     17
@@ -187,15 +157,13 @@ public class BinaryTree<E> implements AbstractBinaryTree<E> {
     }
 
     private void postOrderRecc(BinaryTree<E> current, List<AbstractBinaryTree<E>> result) {
-        if (current == null) {
-            return;
+        if (current.left != null) {
+            postOrderRecc(current.left, result);
         }
 
-        BinaryTree<E> left = current.left;
-        postOrderRecc(left, result);
-
-        BinaryTree<E> right = current.right;
-        postOrderRecc(right, result);
+        if (current.right != null) {
+            postOrderRecc(current.right, result);
+        }
 
         result.add(current);
     }
