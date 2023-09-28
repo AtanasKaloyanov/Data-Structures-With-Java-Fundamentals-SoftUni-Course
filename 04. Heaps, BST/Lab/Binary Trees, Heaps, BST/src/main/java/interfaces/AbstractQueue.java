a4fb3d89006fb8742a3cff8e3d819b0fb039b886
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2023-09-28 22:50:34 +0300

    Heaps and BST deleting project

--- 04. Heaps, BST/Lab/Binary Trees, Heaps, BST/src/main/java/interfaces/AbstractQueue.java
+++
@@ -1,8 +0,0 @@
-package interfaces;
-
-public interface AbstractQueue<E extends Comparable<E>> {
-    int size();
-    void add(E element);
-    E peek();
-    E poll();
-}
--- 04. Heaps, BST/Lab/Binary Trees, Heaps, BST/src/main/java/interfaces/Heap.java
+++
@@ -1,7 +0,0 @@
-package interfaces;
-
-public interface Heap<E extends Comparable<E>> {
-    int size();
-    void add(E element);
-    E peek();
-}
